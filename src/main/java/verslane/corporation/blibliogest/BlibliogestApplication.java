749de723919669302c130e71f68832abe54fec61
Verslane <thesliverbad@gmail.com>
2020-04-17 18:03:21 +0200

    Start up BliblioGest application & initialize settings

---
+++ src/main/java/verslane/corporation/blibliogest/BlibliogestApplication.java
@@ -0,0 +1,13 @@
+package verslane.corporation.blibliogest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlibliogestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlibliogestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/verslane/corporation/blibliogest/BlibliogestApplicationTests.java
@@ -0,0 +1,13 @@
+package verslane.corporation.blibliogest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlibliogestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
